971c6ead8282dd3c82591ecf5f930957176600d1
missopi <missopi@me.com>
2023-08-24 21:20:34 +0100

    Add test for new_board to be empty

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
 require_relative '../lib/board'
+
+# class for creating and updating the board
+describe Board do
+  describe '#initialize' do
+    subject(:new_board) { described_class.new }
+
+    matcher :be_empty do
+      match { |cell| cell == ' ' }
+    end
+
+    context 'when a new board is created' do
+      xit 'is an empty board' do
+        board_cell = new_board.board
+        expect(board_cell).to all(be_empty)
+      end
+    end
+  end
+
+end

bb8cde4834696baafa49bf8ead9c6a6482e3cefc
missopi <missopi@me.com>
2023-08-24 20:58:56 +0100

    Add method name to test for better readability

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,15 +5,17 @@ require_relative '../lib/player'
 describe Player do
   subject(:new_player) {described_class.new('Sophie', 'red')}
 
-  context 'when a new player is created' do
-    it 'returns the player name' do
-      player_name = new_player.name
-      expect(player_name).to eq('Sophie')
-    end
+  describe '#initialize' do
+    context 'when a new player is created' do
+      it 'returns the player name' do
+        player_name = new_player.name
+        expect(player_name).to eq('Sophie')
+      end
 
-    it 'returns the player token' do
-      player_token = new_player.token
-      expect(player_token).to eq('red')
+      it 'returns the player token' do
+        player_token = new_player.token
+        expect(player_token).to eq('red')
+      end
     end
   end
 end

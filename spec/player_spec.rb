4cf834b4b4c99ae6b1f070b4e373c35f48ba011e
missopi <missopi@me.com>
2023-08-24 20:44:17 +0100

    Simplify player test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,13 +3,12 @@
 require_relative '../lib/player'
 
 describe Player do
-  let(:get_token) { double('token', value: 'red')}
-  subject(:player) {described_class.new('Sophie', get_token)}
+  subject(:new_player) {described_class.new('Sophie', 'red')}
 
   context 'when a new player is created' do
-    xit 'it is assigned a token' do
-      assigned_token = player.token
-      expect(assigned_token).to eq('red')
+    it 'it returns the player name and token' do
+      player = new_player
+      expect(player).to eq('Sophie', 'red')
     end
   end
 end

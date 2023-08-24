8ebff521bd94bed6f75d57f05b9273cebab08bbe
missopi <missopi@me.com>
2023-08-24 20:50:28 +0100

    Seperate the tests for returning name and token

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,9 +6,14 @@ describe Player do
   subject(:new_player) {described_class.new('Sophie', 'red')}
 
   context 'when a new player is created' do
-    it 'it returns the player name and token' do
-      player = new_player
-      expect(player).to eq('Sophie', 'red')
+    it 'returns the player name' do
+      player_name = new_player.name
+      expect(player_name).to eq('Sophie')
+    end
+
+    it 'returns the player token' do
+      player_token = new_player.token
+      expect(player_token).to eq('red')
     end
   end
 end

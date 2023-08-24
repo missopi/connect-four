54785ae27e4d57abae184fd3537ccbf85414c759
missopi <missopi@me.com>
2023-08-24 17:03:43 +0100

    Add test for player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,15 @@
-require_relative './lib/player'
+# frozen_string_literal: true
+
+require_relative '../lib/player'
+
+describe Player do
+  let(:get_token) { double('token', value: 'red')}
+  subject(:player) {described_class.new('Sophie', get_token)}
+
+  context 'when a new player is created' do
+    xit 'it is assigned a token' do
+      assigned_token = player.token
+      expect(assigned_token).to eq('red')
+    end
+  end
+end

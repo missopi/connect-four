586c7ab3f7108c9444f03ec473a6b60255e35235
missopi <missopi@me.com>
2023-08-24 22:34:58 +0100

    Adjust wording of test and complete so all pass

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,26 +7,21 @@ describe Board do
   describe '#initialize' do
     subject(:new_board) { described_class.new }
 
-    matcher :be_empty do
-      match { |cell| cell == ' ' }
-    end
-
     context 'when a new board is created' do
-      xit 'is an empty board' do
-        board_cells = new_board.board
-        expect(board_cells).to all(be_empty)
+      it 'is an empty board' do
+        board_cells = new_board.board[0]
+        expect(board_cells).to all(be_nil)
       end
 
-      xit 'has 6 rows' do
+      it 'has 6 rows' do
         rows = new_board.board.size
         expect(rows).to eq(6)
       end
 
-      xit 'has 7 columns' do
+      it 'has 7 columns' do
         columns = new_board.board[0].size
         expect(columns).to eq(7)
       end
     end
   end
-
 end

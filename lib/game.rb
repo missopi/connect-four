a7af269d9c6467ee287b8e9fa64955fbdc031c08
missopi <missopi@me.com>
2023-08-25 16:08:53 +0100

    Fix links

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative '../board'
-require_relative '../player'
+require_relative '../lib/board'
+require_relative '../lib/player'
 require 'colorize'
 
 # class for the gameplay

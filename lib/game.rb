312bbb54bf75469c5b5ba026c4611b068714b9b6
missopi <missopi@me.com>
2023-08-25 16:20:11 +0100

    Layout plan for methods and tests

--- lib/game.rb
+++ lib/game.rb
@@ -6,4 +6,33 @@ require 'colorize'
 
 # class for the gameplay
 class Game
+  def initialize
+  end
+
+  def instructions
+  end
+
+  def play
+  end
+
+  def player_turn
+  end
+
+  def player_input
+  end
+
+  def valid_move
+  end
+
+  def game_over?
+  end
+
+  def win
+  end
+
+  def draw
+  end
+
+  def final_message
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,32 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+
+describe Game do
+  describe '#initialize' do
+  end
+
+  describe '#play' do
+  end
+
+  describe '#player_turn' do
+  end
+
+  describe '#player_input' do
+  end
+
+  describe '#valid_move' do
+  end
+
+  describe 'game_over?' do
+  end
+
+  describe 'wim' do
+  end
+
+  describe 'draw' do
+  end
+
+  describe '#final_message' do
+  end
+end

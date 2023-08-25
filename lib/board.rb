d7281cdbd2410701740bf8213c2d4b885a6c99ee
missopi <missopi@me.com>
2023-08-25 10:32:20 +0100

    Update board class to work with tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 class Board
   attr_accessor :board
 
   def initialize
     @board = Array.new(6) { Array.new(7) }
+  end
+
+  def update_board(row, column, token)
+    board[row][column] = token
+  end
+
+  def red_token
+    ' ● '.colorize(:color => :red)
+  end
+
+  def yellow_token
+    ' ● '.colorize(:color => :yellow)
   end
 end

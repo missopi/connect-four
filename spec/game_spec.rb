7c40cc86dacfad37099f399717e10aa832d74ba4
missopi <missopi@me.com>
2023-08-27 16:24:02 +0100

    Remove unnecessary tests
    Player.new already previously tested in player_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,39 +3,6 @@
 require_relative '../lib/game'
 
 describe Game do
-  describe '#initialize' do
-  end
-
-  describe '#create_player' do
-    subject(:game_player) { described_class.new(player_one, player_two) }
-    let(:player_one) { double(Player, name: 'Sophie', token: ' O ') }
-    let(:player_two) { double(Player, name: 'Max', token: ' X ') }
-
-    context 'when creating player one' do
-      before do
-        allow(game_player).to receive(:puts)
-        allow(game_player).to receive(:gets).and_return('Sophie')
-      end
-
-      xit 'is created with correct parameters' do
-        expect(player_one).to receive(:new).with('Sophie', ' O')
-        game_player.create_player(1)
-      end
-    end
-
-    context 'when creating player two' do
-      before do
-        allow(game_player).to receive(:puts)
-        allow(game_player).to receive(:gets).and_return('Max')
-      end
-
-      xit 'is created with correct parameters' do
-        expect(player_two).to receive(:new).with('Max', ' X')
-        game_player.create_player(2)
-      end
-    end
-  end
-
   describe '#play' do
   end
 
